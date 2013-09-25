d92946b554119feb1e99ae768a83c7a179ce237b
Nicolas Dudebout <nicolas.dudebout@gatech.edu>
2013-09-25 15:27:55 -0400

    Adds things

--- init.el
+++ init.el
@@ -13,6 +13,8 @@
 (require 'cask "~/external/cask.el/cask.el")
 (cask-setup "~/repositories/emacs_init")
 (epl-initialize)
+;; will work once in .emacs.d
+(require 'pallet)
 
 (ddb/conf/general-behavior)
 (ddb/conf/copy-cut-line-at-point)
@@ -54,19 +56,23 @@
 
 ; Add a debug statement for explicit statement of what is loaded exactly
 (defmacro auto-use-package (package-name &rest args)
-  (cl-letf ((auto-config (auto-use-package-symbol "config" package-name))
-            (auto-init (auto-use-package-symbol "init" package-name)))
+  (cl-letf ((auto-init (auto-use-package-symbol "init" package-name))
+            (auto-config (auto-use-package-symbol "config" package-name)))
     (let* ((auto-bind (auto-use-package-symbol "bind" package-name))
+           (auto-mode (auto-use-package-symbol "mode" package-name))
            (auto-init/arg (when (fboundp auto-init)
                             `(:init (,auto-init))))
            (auto-config/arg (when (fboundp auto-config)
                             `(:config (,auto-config))))
            (auto-bind/arg (when (boundp auto-bind)
-                            `(:bind ,auto-bind))))
+                            `(:bind ,auto-bind)))
+           (auto-mode/arg (when (boundp auto-mode)
+                            `(:mode ,auto-mode))))
       `(use-package ,package-name
-         ,@auto-bind/arg
          ,@auto-init/arg
          ,@auto-config/arg
+         ,@auto-bind/arg
+         ,@auto-mode/arg
          ,@args))))
 
 (setq auto-use-package-symbol-string "ddb/%s/%s")
@@ -95,10 +101,15 @@
 (auto-use-package window-number)
 (auto-use-package yasnippet)
 
-(auto-use-package octave-mod :mode ("\\.m\\'" . octave-mode))
-(auto-use-package less-css-mode :mode ("\\.m\\'" . less-css-mode))
-(auto-use-package yaml-mode :mode ("\\.yaml\\'" . yaml-mode))
+(setq ddb/mode/octave-mod '("\\.m\\'" . octave-mode))
+(setq ddb/mode/less-css-mode '("\\.less\\'" . less-css-mode))
+(setq ddb/mode/yaml-mode '("\\.yaml\\'" . yaml-mode))
+(setq ddb/mode/markdown-mode '("\\.md\\'" . markdown-mode))
 
+(auto-use-package octave-mod)
+(auto-use-package less-css-mode)
+(auto-use-package yaml-mode)
+(auto-use-package markdown-mode)
 
 (ddb/conf/shakespearean)
 (ddb/conf/changelog)
